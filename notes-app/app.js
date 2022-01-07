dea48d64c55862ea45dafa79d79400692b138149
brainplow <67734871+brainplow618@users.noreply.github.com>
2022-01-07 11:40:52 +0500

    install nodemon server

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,14 @@
-const fs = require('fs');
-fs.writeFileSync('notes.txt', 'This file is created by node.js app..YAhoo..!\n')
-fs.appendFileSync('notes.txt', 'I live in FSD')
+
+// const fs = require('fs');
+// fs.writeFileSync('notes.txt', 'This file is created by node.js app..YAhoo..!\n')
+// fs.appendFileSync('notes.txt', 'I live in FSD')
+
+// const fname = require('./index.js')
+// console.log(fname)
+
+
+const {add,sub}  = require('./index.js')
+const sum  = add(4, 3)
+const diff  = sub(4, 3)
+console.log(sum)
+console.log(diff)
---
+++ notes-app/index.js
@@ -0,0 +1,19 @@
+// console.log('Every thing is possible')
+
+// console.log('index.js')
+
+// const name = 'shami'
+// module.exports = name
+
+const add = function (a, b) {
+    return a + b
+}
+
+const sub = function (a, b) {
+    return a - b
+}
+
+module.exports = {
+     add,
+     sub
+    }
